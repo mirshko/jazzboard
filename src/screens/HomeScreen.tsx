17ce6787552a279f09b02c546ad7fffb8efb379f
Jeff Reiner <theundeadghost@gmail.com>
2023-12-18 21:31:52 +0100

    Adding more routes and data payload

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,31 @@
 import { Button } from "@/basicComponents";
 import { Link } from "react-router-dom";
+import { TodoAccountRoot } from "@/types";
+import { useJazz } from "jazz-react";
+import { Profile } from "cojson";
 
 export function HomeScreen() {
-  return <>
-    <Button asChild variant="outline">
-      <Link to="/projects">Projects</Link>
-    </Button >
-  </>
+  const { me } = useJazz<Profile, TodoAccountRoot>();
+
+  return (
+    <>
+      <pre>
+        <code>{JSON.stringify(me.root, null, 2)}</code>
+      </pre>
+
+      <ul className="space-y-4">
+        <li>
+          <Button asChild variant="outline">
+            <Link to="/projects">Projects</Link>
+          </Button>
+        </li>
+
+        <li>
+          <Button asChild variant="outline">
+            <Link to="/bookmarks">Bookmarks</Link>
+          </Button>
+        </li>
+      </ul>
+    </>
+  );
 }
